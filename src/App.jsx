ef05a8b9c3caae4723a6f494f643bff7c9ee8a09
Rohith <rohithr4208@gmail.com>
2025-07-19 15:36:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + ROHITH</h1>
+      <h1>Good Bad Ugly</h1>
+      <p>AK Oru Red Dragon</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
